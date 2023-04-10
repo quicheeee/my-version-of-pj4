895efc5cb89afd9f954aef952664b9cbab051cc5
barnet76 <129809416+barnet76@users.noreply.github.com>
2023-04-10 16:23:57 -0400

    Added comments

--- Customer.java
+++ Customer.java
@@ -4,18 +4,19 @@ import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+
 public class Customer extends User implements Serializable {
     private ArrayList<Store> storeList;
-
+    // Customer Class Constructor takes in values extended from User class.
     public Customer(String name, String email, String password) {
         super(name, email, password);
 
     }
-
+    // returns store list
     public ArrayList<Store> getStoreList() {
         return storeList;
     }
-
+    // sets the store list 
     public void setStoreList(ArrayList<Store> storeList) {
         this.storeList = storeList;
     }
@@ -35,6 +36,7 @@ public class Customer extends User implements Serializable {
     }
     
      */
+    // adds desired store to list of stores
     public void addStore(Store s) {
         storeList.add(s);
     }
@@ -49,6 +51,7 @@ public class Customer extends User implements Serializable {
         return sellerEmail;
     }
      */
+    // returns desired seller from list of users
     public Seller getSeller(int index) {
         File f = new File("accounts.ser");
         int count = 1;
@@ -64,9 +67,10 @@ public class Customer extends User implements Serializable {
                 }
             }
         }
-        return null;
+        return null; // else returns a null value
     }
-
+    
+    // displays all sellers found in getUsers()
     public void displaySellers() {
         File f = new File("accounts.ser");
         int count = 1;
@@ -86,8 +90,8 @@ public class Customer extends User implements Serializable {
         }
     }
 
-    // fix equals
-
+    
+    @Override // checks for customer objects with equal values
     public boolean equals(Object o) {
         if (o instanceof Customer) {
             Customer customer = (Customer) o;
@@ -97,7 +101,8 @@ public class Customer extends User implements Serializable {
         }
         return false;
     }
-
+    
+    // method determines that customer may only message sellers
     public boolean canMessage(Object o) {
         if (o instanceof Seller) {
             return true;
