cc59ab905e4b7c18c43e249a294e22deb85743c0
barnet76 <129809416+barnet76@users.noreply.github.com>
2023-04-10 16:13:53 -0400

    Update Messenger.java

--- Messenger.java
+++ Messenger.java
@@ -1,144 +1,178 @@
-package pj4;
-
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.*;
 
 public class Messenger {
-	private static final String FILENAME = "messages.ser";
-	private static ArrayList<Message> messages = null;
+    private static final String FILENAME = "messages.ser";
+    private static ArrayList<Message> messages = null;
 
-	public static ArrayList<Message> getMessages() {
-		if (Messenger.messages != null)
-			return Messenger.messages;
+    public static ArrayList<Message> getMessages() {
+        if (Messenger.messages != null)
+            return Messenger.messages;
 
-		ArrayList<Message> temp = new ArrayList<Message>();
+        ArrayList<Message> temp = new ArrayList<Message>();
 
-		try {
-			File f = new File(FILENAME);
-			FileInputStream fis;
-			ObjectInputStream ois;
+        try {
+            File f = new File(FILENAME);
+            FileInputStream fis;
+            ObjectInputStream ois;
 
-			try {
-				fis = new FileInputStream(f);
-				ois = new ObjectInputStream(fis);
+            try {
+                fis = new FileInputStream(f);
+                ois = new ObjectInputStream(fis);
 
-				while (true) {
-					Message m = (Message) ois.readObject();
-					temp.add(m);
-				}
-			} catch (EOFException ex) {
-			} catch (FileNotFoundException ex2) {
-			}
+                while (true) {
+                    Message m = (Message) ois.readObject();
+                    temp.add(m);
+                }
+            } catch (EOFException ex) {
+            } catch (FileNotFoundException ex2) {
+            }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		Messenger.messages = temp;
-		return Messenger.messages;
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Messenger.messages = temp;
+        return Messenger.messages;
+    }
 
-	public static void sendNewMessage(User sender, User receiver, String message, Boolean disappearing) {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		String dateString = dtf.format(LocalDateTime.now());
+    public static void sendNewMessage(User sender, User receiver, String message, Boolean disappearing) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String dateString = dtf.format(LocalDateTime.now());
 
-		Message m1 = new Message(sender, sender, receiver, message, disappearing, dateString);
-		m1.setRead(true);
-		Message m2 = new Message(receiver, sender, receiver, message, disappearing, dateString);
+        Message m1 = new Message(sender, sender, receiver, message, disappearing, dateString);
+        m1.setRead(true);
+        Message m2 = new Message(receiver, sender, receiver, message, disappearing, dateString);
 
-		ArrayList<Message> temp = Messenger.getMessages();
-		temp.add(m1);
-		temp.add(m2);
-		Messenger.writeMessages();
-	}
+        ArrayList<Message> temp = Messenger.getMessages();
+        temp.add(m1);
+        temp.add(m2);
+        Messenger.writeMessages();
+    }
 
-	public static void writeMessages() {
-		try {
-			File f = new File(Messenger.FILENAME);
-			FileOutputStream fos = new FileOutputStream(f);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
+    public static void writeMessages() {
+        try {
+            File f = new File(Messenger.FILENAME);
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-			for (Message m : Messenger.messages) {
-				oos.writeObject(m);
-			}
-			oos.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            for (Message m : Messenger.messages) {
+                oos.writeObject(m);
+            }
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static ArrayList<Message> getMessagesForUser(User u) {
-		ArrayList<Message> temp = Messenger.getMessages();
-		ArrayList<Message> results = new ArrayList<Message>();
-		for (Message m : temp) {
-			if (m.getMessageOwner().equals(u)) {
-				User other;
-				if (!m.getSender().equals(u))
-					other = m.getSender();
-				else
-					other = m.getReceiver();
+    public static ArrayList<Message> getMessagesForUser(User u) {
+        ArrayList<Message> temp = Messenger.getMessages();
+        ArrayList<Message> results = new ArrayList<Message>();
+        for (Message m : temp) {
+            if (m.getMessageOwner().equals(u)) {
+                User other;
+                if (!m.getSender().equals(u))
+                    other = m.getSender();
+                else
+                    other = m.getReceiver();
 
-				if (!(other.getBlockedUsers().contains(u) || u.getBlockedUsers().contains(other)))
-					results.add(m);
-			}
-		}
-		return results;
-	}
+                if (!(other.getBlockedUsers().contains(u) || u.getBlockedUsers().contains(other)))
+                    results.add(m);
+            }
+        }
+        return results;
+    }
 
-	public static void editMessage(Message m, String newMessage) {
-		StringBuilder sb = new StringBuilder(m.getMessage());
-		sb.append("\n");
-		sb.append(newMessage);
+    public static void editMessage(Message m, String newMessage) {
+        StringBuilder sb = new StringBuilder(m.getMessage());
+        sb.append("\n");
+        sb.append(newMessage);
 
-		Message rel = Messenger.findRelatedMessage(m);
-		m.setMessage(sb.toString());
-		m.setRead(true);
-		if (rel != null) {
-			rel.setMessage(sb.toString());
-			rel.setRead(false);
-		}
-		Messenger.writeMessages();
-	}
+        Message rel = Messenger.findRelatedMessage(m);
+        m.setMessage(sb.toString());
+        m.setRead(true);
+        if (rel != null) {
+            rel.setMessage(sb.toString());
+            rel.setRead(false);
+        }
+        Messenger.writeMessages();
+    }
 
-	private static Message findRelatedMessage(Message m) {
-		ArrayList<Message> temp = Messenger.getMessages();
-		for (Message rel : temp) {
-			if (rel.related(m))
-				return rel;
-		}
-		return null;
-	}
+    private static Message findRelatedMessage(Message m) {
+        ArrayList<Message> temp = Messenger.getMessages();
+        for (Message rel : temp) {
+            if (rel.related(m))
+                return rel;
+        }
+        return null;
+    }
 
-	public static void deleteMessage(Message m) {
-		ArrayList<Message> temp = Messenger.getMessages();
-		temp.remove(m);
-		writeMessages();
-	}
+    public static void deleteMessage(Message m) {
+        ArrayList<Message> temp = Messenger.getMessages();
+        temp.remove(m);
+        writeMessages();
+    }
 
-	public static void deleteMessagesForUser(User user) {
-		ArrayList<Message> temp = Messenger.getMessages();
-		ArrayList<Message> removeList = new ArrayList<Message>();
+    public static void deleteMessagesForUser(User user) {
+        ArrayList<Message> temp = Messenger.getMessages();
+        ArrayList<Message> removeList = new ArrayList<Message>();
 
-		for (Message m : temp) {
-			if (m.getSender().equals(user) || m.getReceiver().equals(user))
-				removeList.add(m);
-		}
+        for (Message m : temp) {
+            if (m.getSender().equals(user) || m.getReceiver().equals(user))
+                removeList.add(m);
+        }
 
-		for (Message m : removeList) {
-			temp.remove(m);
-		}
-		writeMessages();
-	}
+        for (Message m : removeList) {
+            temp.remove(m);
+        }
+        writeMessages();
+    }
 
-	public static boolean existsUnreadMessagesForUser(User user) {
-		ArrayList<Message> temp = Messenger.getMessages();
-		for (Message m : temp) {
-			if (!m.isRead())
-				return true;
-		}
-		return false;
-	}
+    public static boolean existsUnreadMessagesForUser(User user) {
+        ArrayList<Message> temp = Messenger.getMessages();
+        for (Message m : temp) {
+            if (!m.isRead())
+                return true;
+        }
+        return false;
+    }
+
+    public static List<String[]> createCsvData(User u) {
+        List<String[]> list = new ArrayList<>();
+        String [] headers = {"Timestamp", "Sender", "Receiver", "Message"};
+        list.add(headers);
+        for(Message m: getMessagesForUser(u)){
+            String [] msg = {m.getCreateDate(), m.getSender().getName(), m.getReceiver().getName(), m.getMessage()};
+            list.add(msg);
+        }
+        return list;
+    }
+
+    public static void convertToCSV(String fileName, User u){
+        File csvOutput = new File(fileName);
+        try{
+            FileWriter fw = new FileWriter(csvOutput);
+            PrintWriter pw = new PrintWriter(fw);
+            for(String[] s: createCsvData(u)){
+                int count = 1;
+                for(String s1: s){
+                    if(count != 4) {
+                        pw.print(s1);
+                        pw.print(",");
+                        count++;
+                    } else{
+                        pw.println(s1);
+                    }
+                }
+            }
+            pw.close();
+        } catch (FileNotFoundException fnfe){
+        } catch(IOException ioe){
+
+        }
+    }
 
 }
 
