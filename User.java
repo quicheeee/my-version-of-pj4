1d8b543ab45b2990d6486c2aee41f966487ad55c
quicheeee <122551474+quicheeee@users.noreply.github.com>
2023-04-11 14:10:54 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,59 +1,73 @@
 package pj4;
 import java.io.*;
 import java.util.*;
+/**
+ * User
+ *
+ * This class creates a basic user for the application, with a name, email, and password associated.
+ * It writes the users to file for storage after the program is terminated.
+ *
+ * @author Amelia Williams, Meha Kavoori, Anish Puri, Tyler Barnett
+ *
+ * @version 04/10/2023
+ *
+ */
 public class User implements Serializable {
-    private String name;
-    private String email;
-    private String password;
-    //private int userID; //?
-    private ArrayList<User> blockedUsers;
-    //private ArrayList<User> blockedBy;
-    //private ArrayList<User> invisibleTo;
-    //private ArrayList<Messenger2> conversations;
+    private String name; //the name of the user
+    private String email; //the email address of the user
+    private String password; //the password of the user
 
-    //private static final long serialVersionUID = 0;
+    private ArrayList<User> blockedUsers;
+    private ArrayList<String> filters;
+
     private static ArrayList<User> allUsers = null;
     private static final long serialVersionUID = -5554757084812506737L;
 
+    //Constructor to create a user given their name, email, and password
     public User(String name, String email, String password){
         this.name = name;
         this.email = email;
         this.password = password;
         blockedUsers = new ArrayList<User>();
-        //userID = getNumCreated() + 1;
-        //blockedBy = new ArrayList<User>();
-        //invisibleTo = new ArrayList<User>();
-        //conversations = new ArrayList<Messenger2> ();
+        filters = new ArrayList<String>();
     }
 
+    //returns the User's email
     public String getEmail() {
         return email;
     }
 
+    //sets the email field to the inputted string
     public void setEmail(String email) {
         this.email = email;
     }
 
+    //gets the User's password
     public String getPassword() {
         return password;
     }
 
+    //sets the User's password
     public void setPassword(String password) {
         this.password = password;
     }
 
+    //returns the User's name
     public String getName() {
         return name;
     }
 
+    //returns an ArrayList of Users blocked by the given User
     public ArrayList<User> getBlockedUsers(){
         return blockedUsers;
     }
 
+    //sets the stored ArrayList of Users blocked by the given User to the inputted ArrayList
     public void setBlockedUsers(ArrayList<User> blockedUsers){
         this.blockedUsers = blockedUsers;
     }
 
+    //this method is used to verify login information when a user signs into the application
     public static User signIn (String email, String password) {
         ArrayList<User> users = getUsers();
         for(User u: users) {
@@ -67,24 +81,7 @@ public class User implements Serializable {
         return null;
     }
 
-/*
-    public static User findUser(int userID){
-        try{
-            File accounts = new File("accounts.ser");
-            ArrayList<User> users = listUsers(accounts, getNumCreated());
-
-            for(User u: users) {
-                if(u.getUserID() == userID) {
-                    return u;
-                }
-            }
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-*/
-
+    //a static method used to identify a User object from the associated email address
     public static User findUserWithEmail(String email){
         try{
             ArrayList<User> users = getUsers();
@@ -100,6 +97,8 @@ public class User implements Serializable {
         return null;
     }
 
+    //a static method that returns an arraylist of sellers that are not blocked
+    //whose email address contains a search term inputted by the given sender
     public static ArrayList<User> searchSellerByUser(String search, User sender) {
         ArrayList<User> users = getUsers();
         ArrayList<User> results = new ArrayList<User>();
@@ -114,6 +113,8 @@ public class User implements Serializable {
         return results;
     }
 
+    //a static method that returns an arraylist of customers that are not blocked
+    //whose email address contains a search term inputted by the given sender
     public static ArrayList<User> searchCustomerByUser(String search, User sender) {
         ArrayList<User> users = getUsers();
         ArrayList<User> results = new ArrayList<User>();
@@ -128,6 +129,7 @@ public class User implements Serializable {
         return results;
     }
 
+    //a static method that returns an arraylist of customers that are not blocked by the given sender
     public static ArrayList<User> getCustomersByUser(User sender) {
         ArrayList<User> users = getUsers();
         ArrayList<User> results = new ArrayList<User>();
@@ -141,6 +143,8 @@ public class User implements Serializable {
         return results;
     }
 
+    //creates a new user when the appropriate fields are inputted and creates a customer or seller object
+    //and then writes the user to file for storage of information
     public static boolean newUser(String name, String emailAddress, String password, String storeName, int userType) {
         ArrayList<User> users = User.getUsers();
         for(User u: users) {
@@ -156,6 +160,7 @@ public class User implements Serializable {
             u = new Customer(name, emailAddress, password);
             System.out.println("Customer Created");
         } else if (userType == 2) {
+            //Seller Account
             u = new Seller(name, emailAddress, password);
             ((Seller) u).addStore(storeName);
             System.out.println("Seller Created");
@@ -170,9 +175,10 @@ public class User implements Serializable {
 
     public static void addNewStore(Seller seller, String storeName) {
         seller.addStore(storeName);
-        User.writeUsers(User.allUsers);
+        User.writeUsers(User.getUsers());
     }
 
+    //given an ArrayList of Users, this method will write them to the file which stores them ("accounts.ser")
     private static void writeUsers(ArrayList<User> users){
         try{
             File f = new File("accounts.ser");
@@ -188,6 +194,7 @@ public class User implements Serializable {
         }
     }
 
+    //this method returns an ArrayList of Users with all of the users in the file "accounts.ser"
     public static ArrayList<User> getUsers() {
         if (User.allUsers != null)
             return  User.allUsers;
@@ -209,10 +216,6 @@ public class User implements Serializable {
             } catch (EOFException ex) {
             }
 
-//            for (int i = 0; i < fileLength; i++){
-//                User u = (User) ois.readObject();
-//                users.add(u);
-//            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -220,11 +223,13 @@ public class User implements Serializable {
         return User.allUsers;
     }
 
+    //this method blocks an inputted user
     public void block(User user) {
         this.blockedUsers.add(user);
         writeUsers(User.getUsers());
     }
 
+    @Override
     public boolean equals(Object o){
         if(o instanceof User){
             User u = (User) o;
@@ -235,12 +240,38 @@ public class User implements Serializable {
         return false;
     }
 
+    //this methhod deletes a user, removing them from the file "accounts.ser"
     public static void deleteUser(User u) {
         Messenger.deleteConversationsForUser(u);
 
         ArrayList<User> temp = User.getUsers();
         temp.remove(u);
         writeUsers(temp);
+    }
+
+    public ArrayList<String> getFilters() {
+        if (filters == null)
+            filters = new ArrayList<String>();
+        return filters;
+    }
+
+    public void addFilter(String original, String replacement) {
+        if((original == null) || (replacement == null))
+            throw new IllegalArgumentException("Invalid filter strings");
+
+        filters.add(original);
+        filters.add(replacement);
+        User.writeUsers(User.getUsers());
+    }
+
+    public String applyFilters(String message) {
+        if (filters == null)
+            return message;
+
+        for (int i = 0; i < filters.size(); i += 2) {
+            message = message.replace(filters.get(i), filters.get(i+1));
+        }
+        return message;
     }
 
 }
