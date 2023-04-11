f8eb6b2897ee532805ee6c8cf34723d1f49a32a2
barnet76 <129809416+barnet76@users.noreply.github.com>
2023-04-10 21:58:49 -0400

    Added comments

--- Customer.java
+++ Customer.java
@@ -3,7 +3,14 @@ package pj4;
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
-
+/**
+ * Customer Class
+ * The customer class represents a Customer object which extends User.
+ * The customer has a list of stores in addition to all User methods.
+ *
+ * @author Amelia Williams, Meha Kavoori, Anish Puri, Tyler Barnett
+ * @version 4/10/2023
+ */
 
 public class Customer extends User implements Serializable {
     private ArrayList<Store> storeList;
@@ -21,36 +28,12 @@ public class Customer extends User implements Serializable {
         this.storeList = storeList;
     }
 
-
-    /*public void addSeller(Seller c) {
-        ArrayList<Seller> arr = new ArrayList<>();
-        for (int i = 0; i < storeList.length; i++) {
-            arr.set(i, this.storeList[i]);
-        }
-
-        arr.add(c);
-
-        for (int i = 0; i < arr.size(); i++) {
-            this.storeList[i] = arr.get(i);
-        }
-    }
-    
-     */
     // adds desired store to list of stores
     public void addStore(Store s) {
         storeList.add(s);
     }
 
-    /*public String searchSeller(int sellerID) {
-        String sellerEmail = "Customer Not Found";
-        for (int i = 0; i < storeList.length; i++) {
-            if (storeList[i].getSellerID() == sellerID) {
-                sellerEmail = storeList[i].getEmail();
-            }
-        }
-        return sellerEmail;
-    }
-     */
+    
     // returns desired seller from list of users
     public Seller getSeller(int index) {
         File f = new File("accounts.ser");
@@ -77,20 +60,12 @@ public class Customer extends User implements Serializable {
         for (User u : getUsers()) {
             if (u instanceof Seller) {
                 if (!this.getBlockedUsers().contains(u)) {
-/*
-                    if (!this.getBlockedBy().contains(u)) {
-                        if (!u.getInvisibleTo().contains(this)) {
-                            System.out.println(count++ + ". " + u.getEmail());
 
-                        }
-                    }
-*/
                 }
             }
         }
     }
 
-    
     @Override // checks for customer objects with equal values
     public boolean equals(Object o) {
         if (o instanceof Customer) {
@@ -109,7 +84,7 @@ public class Customer extends User implements Serializable {
         }
         return false;
     }
-    //public Message newMessage (); // in progress
+    
 
 
 }
